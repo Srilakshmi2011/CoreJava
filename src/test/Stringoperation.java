8b11f08289dbe6af1a2c7294e3bfba1f8fa3f438
srilakshmi2468 <srilakshmi.devarapalli@gmail.com>
2024-01-05 03:58:43 +0000

    commiting core java file

---
+++ src/test/Stringoperation.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Stringoperation {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+   String str1="clean India Green India";
+   System.out.println("result is"  + str1.length());
+   System.out.println("First char is " + str1.charAt(0));
+	
+	System.out.println("Last char is " + str1.charAt(str1.length()-1));
+	
+	System.out.println("The upper case string is " + str1.toUpperCase());
+
+}
+	
+
+}
